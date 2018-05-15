6e51b72ab9aec3727e5a87642118a1d57a5d34c0
Shane Smith <shane.smith.2@Aonhewitt.com>
2018-05-15 16:29:12 -0500

    Start to React Library project

---
+++ src/components/HelloWorld/HelloWorld.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import './HelloWorld.css';
+
+export interface IProps {
+    /**
+     * Message to append to component
+     */
+    message?: string;
+}
+
+export default class HelloWorld extends React.Component<IProps, { }> {
+
+    public static defaultProps: IProps = {
+        message: 'World'
+    };
+
+    constructor(props: IProps) {
+        super(props);
+    }
+
+    public render() {
+
+        return (
+            <div>
+                Hello {this.props.message}
+            </div>
+        );
+    }
+}
